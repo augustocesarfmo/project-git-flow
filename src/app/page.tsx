4edf2497799fe7858093e64440254abef5dd032c
Augusto Oliveira <augustocesarfmo@gmail.com>
2023-11-13 15:30:17 -0300

    alteracoes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,17 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>Branch adriel</main>;
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Meu cabeçalho</h1>
+        <ul>
+          <li>Item</li>
+          <li>Item</li>
+          <li>Item</li>
+          <li>Item</li>
+        </ul>
+      </div>
+    </main>
+  );
 }
